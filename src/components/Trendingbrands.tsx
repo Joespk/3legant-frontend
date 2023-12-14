005b2fa05de16a99814cb0d5d18b54a7256f47a5
Joespk <supakritjoe@gmail.com>
2023-12-14 13:35:58 +0700

    add logo img

--- src/components/Trendingbrands.tsx
+++ src/components/Trendingbrands.tsx
@@ -1,10 +1,24 @@
 import classes from "./Trendingbrands.module.css";
 import trendingbrands from "../assets/TrendingBrands.svg";
+import logo1 from "../assets/logo01.svg";
+import logo2 from "../assets/logo02.svg";
+import logo3 from "../assets/logo03.svg";
+import logo4 from "../assets/logo04.svg";
+import logo5 from "../assets/logo05.svg";
+import logo6 from "../assets/logo06.svg";
 
 const Trendingbrands = () => {
   return (
     <div className={classes.container}>
       <img src={trendingbrands} />
+      <div className={classes.logo}>
+        <img src={logo1} />
+        <img src={logo2} />
+        <img src={logo3} />
+        <img src={logo4} />
+        <img src={logo5} />
+        <img src={logo6} />
+      </div>
     </div>
   );
 };
