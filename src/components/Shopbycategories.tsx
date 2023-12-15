d210cf8832057e9a7b8b708165fd1cc45232c289
Joespk <supakritjoe@gmail.com>
2023-12-15 21:14:39 +0700

    add image

--- src/components/Shopbycategories.tsx
+++ src/components/Shopbycategories.tsx
@@ -1,5 +1,10 @@
 import classes from "./Shopbycategories.module.css";
 import puffers from "../assets/Categories/puffers.svg";
+import bombers from "../assets/Categories/Bombers.svg";
+import lightweight from "../assets/Categories/Lightweightjackets.svg";
+import gilets from "../assets/Categories/Gilets.svg";
+import coats from "../assets/Categories/Coats.svg";
+import rainwear from "../assets/Categories/Rainwear.svg";
 
 const Shopbycategories = () => {
   return (
@@ -11,23 +16,23 @@ const Shopbycategories = () => {
           <p>Puffers</p>
         </div>
         <div>
-          <img className={classes.roundimage} src={puffers} />
+          <img className={classes.roundimage} src={bombers} />
           <p>Bombers</p>
         </div>
         <div>
-          <img className={classes.roundimage} src={puffers} />
+          <img className={classes.roundimage} src={lightweight} />
           <p>Lightweight jackets</p>
         </div>
         <div>
-          <img className={classes.roundimage} src={puffers} />
+          <img className={classes.roundimage} src={gilets} />
           <p>Gilets</p>
         </div>
         <div>
-          <img className={classes.roundimage} src={puffers} />
+          <img className={classes.roundimage} src={coats} />
           <p>Coats</p>
         </div>
         <div>
-          <img className={classes.roundimage} src={puffers} />
+          <img className={classes.roundimage} src={rainwear} />
           <p>Rainwear</p>
         </div>
       </div>
